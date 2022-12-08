1b9f070cb9a162c48d9a707d687e69bba2106cdd
denstiny <2254228017@qq.com>
2022-12-08 16:48:22 +0800

    feat: windows

--- lua/packers/windows.lua
+++ lua/packers/windows.lua
@@ -5,4 +5,12 @@ end
 vim.o.winwidth = 10
 vim.o.winminwidth = 10
 vim.o.equalalways = false
-windows.setup()
+windows.setup({
+  ignore = {
+    buftype = { "quickfix" },
+    filetype = { "NvimTree", "neo-tree", "undotree", "gundo", "aerial", "toggleterm" }
+  },
+  animation = {
+    fps = 60,
+  }
+})
